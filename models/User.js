c53d906dac1d0545dfee03236bb3df8b40a7ccc5
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2025-12-17 15:21:30 +0530

    add more roles

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['Admin', 'Manager', 'Vice-manager', 'HR'],
+    enum: ['Admin', 'Manager', 'Vice-manager', 'HR', 'Volunteer', 'Member', 'User'],
     required: true,
   },
 }, {
