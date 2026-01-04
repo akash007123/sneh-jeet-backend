6b74b59c3bb15bd89a1ce754c5e9a87272ae4547
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2026-01-04 19:20:00 +0530

    dsffsd

--- utils/email.js
+++ utils/email.js
@@ -1,10 +1,15 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // or your email service
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
@@ -17,7 +22,7 @@ const sendEmail = async (to, subject, text, html) => {
       text,
       html,
     });
-    console.log('Email sent successfully');
+    console.log('Email sent successfully via Gmail');
   } catch (error) {
     console.error('Error sending email:', error);
     throw error;
