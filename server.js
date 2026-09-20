ed4b8fc98a8c5371160650bd14f049afcba7d4ae
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2026-09-20 14:57:39 +0530

    fix all issues

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', (req, res) => {
   res.send('Sneh Jeet NGO Backend API');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
